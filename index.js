6491094abbdafdc38d4874520797b887bbe46588
ravikant <Ravikantwaghmare@vionsys.com>
2024-11-26 12:19:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config("./env");
+const app = express();
+
+app.get("/get", (req, res) => {
+  res.json({ name: "John", age: 30 });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`); // Print the server port in the console
+});
+
+export default app;
